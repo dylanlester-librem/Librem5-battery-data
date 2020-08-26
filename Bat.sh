d8e33d577f638360c4253f32c1cceab31fae8c32
dylanlester-librem <70072983+dylanlester-librem@users.noreply.github.com>
2020-08-26 11:16:43 -0400

    Add files via upload

--- Bat.sh
+++ Bat.sh
@@ -2,4 +2,5 @@
  chargeHR=$(</sys/class/power_supply/max170xx_battery/capacity)
  seconds=$(date +%s)
  chargeL=$(</sys/class/power_supply/max170xx_battery/charge_now)
- (printf "%u%%,%u,%u\n" $chargeHR $chargeL $seconds) >> /home/purism/Documents/battery_data.csv
+CPU=$(</proc/loadavg)
+ (printf "%u%%,%u,%u,%s\n" $chargeHR $chargeL $seconds ${CPU// /,}) >> /home/purism/Documents/battery_data.csv
